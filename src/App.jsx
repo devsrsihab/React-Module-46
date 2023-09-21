8bd327e07a242901bf41517f3e190e61f6b98c0f
devsrsihab <srsohankhan0@gmail..com>
2023-09-21 19:18:58 +0600

    Practice done fro dynamic form

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,33 @@ import DynamicForm from "./Components/Forms/DynamicForm";
 // import RefForm from "./Components/Forms/RefForm"
 // import SateForm from "./Components/Forms/SateForm"
 
+
+
+
 function App() {
+
+  const handleSignUp = data => {
+
+    console.log(data);
+  }
+
+  const handleLogin = data => {
+    console.log(data);
+  }
+
   return (
     <>
       <main className="flex bg-accent  justify-center items-center h-screen">
-        <div className="container  mx-auto p-10    ">
-          <DynamicForm/>
+        <div className="container flex gap-20  mx-auto p-10    ">
+          <DynamicForm handleData={handleSignUp} btnText={"Sign Up"} formTitle={"Sign Up"} formDesc={"Please Fill up the form for sign up"} >
+          <h1 className="text-3xl mb-3">Sign Up Step 2</h1>
+          <p>Fill up the form for sign up</p> 
+          </DynamicForm>
+          <DynamicForm handleData={handleLogin} btnText={"Login"} formTitle={"Login"} formDesc={"Please Fill up the form for Login"} >
+          <h1 className="text-3xl mb-3">Sigin </h1>
+          <p>Fill up the form for login </p> 
+          </DynamicForm>
+          
         </div>
       </main>
     </>
--- src/Components/Forms/DynamicForm.jsx
+++ src/Components/Forms/DynamicForm.jsx
@@ -1,51 +1,59 @@
+const DynamicForm = ({  handleData, btnText, children }) => {
+  const handleForm = (e) => {
+    e.preventDefault();
 
-const DynamicForm = () => {
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      phone: e.target.phone.value,
+    };
 
-    const handleForm = (e) => {
-      e.preventDefault();
-
-    }
+    handleData(data);
+  };
 
   return (
-    <form onSubmit={handleForm} action="" autoComplete="off">
-    <div className="form-group">
-      <label className="label">
-        <span className="label-text">Name</span>
-      </label>
-      <input
-        name="name"
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-warning w-full max-w-xs"
-      />
+    <div className="form-container  text-white">
+      {children}
+      <form onSubmit={handleForm} action="" autoComplete="off">
+        <div className="form-group">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered input-warning w-full max-w-xs"
+          />
+        </div>
+        <div className="form-group">
+          <label className="label">
+            <span className="label-text">Phone</span>
+          </label>
+          <input
+            name="phone"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered input-warning w-full max-w-xs"
+          />
+        </div>
+        <div className="form-group">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered input-warning w-full max-w-xs"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-5">
+          {btnText}
+        </button>
+      </form>
     </div>
-    <div className="form-group">
-      <label className="label">
-        <span className="label-text">Phone</span>
-      </label>
-      <input
-        name="phone"
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-warning w-full max-w-xs"
-      />
-    </div>
-    <div className="form-group">
-      <label className="label">
-        <span className="label-text">Email</span>
-      </label>
-      <input
-        name="email"
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-warning w-full max-w-xs"
-      />
-    </div>
-    <button type="submit" className="btn btn-primary mt-5">
-      Submit
-    </button>
-  </form>
-  )
-}
+  );
+};
 
-export default DynamicForm
+export default DynamicForm;
